b0a96a64096ec48ed493d709828213f62781f96f
34j <34j.github@proton.me>
2026-05-13 12:40:53 +0900

    chore: wip

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,7 @@
 import math
 from typing import Any
 
+import array_api_extra as xpx
 import pytest
 from array_api._2024_12 import ArrayNamespaceFull
 
@@ -21,11 +22,7 @@ def test_example_13_19(
     interpolant = example_13_19(a, b, n_nodes, xp=xp, device=device, dtype=dtype)
 
     eval_points = xp.random.random_uniform(0, 2 * math.pi, (10,), device=device, dtype=dtype)
-    print(eval_points)
 
-    expected_phi = example_13_19_answer(eval_points)
-
-    phi_computed = interpolant(eval_points)
-
-    error = xp.max(xp.abs(phi_computed - expected_phi))
-    assert float(error) < 1e-6
+    expected = xp.astype(example_13_19_answer(eval_points), xp.result_type(dtype, 1j))
+    actual = interpolant(eval_points)
+    assert xp.all(xpx.isclose(actual, expected))
